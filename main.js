dab4ba3e12b4231b2fb7d1f2bbfaefd8c6fda935
Sofia Nguyen <120623425+symsoph@users.noreply.github.com>
2024-05-11 17:59:19 -0700

    pray jsdoc action works

--- main.js
+++ main.js
@@ -6,4 +6,15 @@ function createTest() {
     return 0
 }
 
+/**
+ * Adding two numbers
+ * @param {Number} num1 
+ * @param {Number} num2 
+ * @returns sum of num1 + num2
+ */
+function hello(num1, num2){
+    let sum = num1 + num2;
+    return sum;
+}
+
 console.log("hello world");
